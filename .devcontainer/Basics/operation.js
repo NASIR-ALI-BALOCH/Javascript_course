2c7f46d7c6a4edb667934a59efe276c312bdc97e
NASIR ALI <162355004+NASIR-ALI-BALOCH@users.noreply.github.com>
2024-03-15 07:20:32 +0500

    operations are completed

---
+++ .devcontainer/Basics/operation.js
@@ -0,0 +1,20 @@
+let value=5
+let negValue=-value
+console.log(negValue)
+console.log(2+2)
+console.log("2"+2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**4)
+console.log("2" +"2");
+console.log("3"+2+2);
+
+////
+let str1="Hello"
+let str2=" Nasir"
+let str3= str1+str2
+console.log(str3);
+
+
+console.log(1+2+"3");
+console.log(+true);
