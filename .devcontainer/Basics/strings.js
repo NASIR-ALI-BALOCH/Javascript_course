cfda0a195f716e94f126b543d61a5673eee4ac01
NASIR ALI <162355004+NASIR-ALI-BALOCH@users.noreply.github.com>
2024-03-16 12:33:37 +0500

    strings

---
+++ .devcontainer/Basics/strings.js
@@ -0,0 +1,8 @@
+const name="nasir"
+const countRepo=56
+//console.table([name ,countRepo])
+console.log(`my name is ${name.toUpperCase()} there is another ${name.toLowerCase()} and repo is ${countRepo}`);
+const gamename=String("nasirali")
+console.log("game name is ",gamename);
+console.log(gamename[0]);
+console.log(gamename.__proto__);   
