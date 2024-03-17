5c3233d40437cd75bbf7f7f164227fb5cea72af6
NASIR ALI <162355004+NASIR-ALI-BALOCH@users.noreply.github.com>
2024-03-17 15:09:17 +0500

    maths functions

---
+++ .devcontainer/Basics/numbermath.js
@@ -0,0 +1,31 @@
+//strings
+const blnc=732
+console.log(blnc);
+const balance = new Number(732)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+
+
+const otherNumber=762.873
+console.log(otherNumber.toPrecision(2));
+const hundreds=10000000
+console.log(hundreds.toLocaleString());
+
+//++++++++++ Maths +++++++++++++++//
+console.log(Math);
+console.log(Math.abs(5));
+
+console.log(Math.random());
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random()+1);
+console.log(Math.random()+1);
+console.log(Math.random()*4+1);
+
+
+
